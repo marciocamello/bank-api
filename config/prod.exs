9cf7ed8a7b78788db19b5fc2f6e5a4bc0ad90bc6
Marcio Camello <marciocamello@outlook.com>
2020-04-19 15:22:12 -0300

    make it better

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :bank_api, BankApi.Repo,
 config :bank_api, ecto_repos: [BankApi.Repo]
 
 # cowboy config
-config :bank_api, cowboy_port: 80
+config :bank_api, cowboy_port: 443
 
 # Configures Elixir's Logger
 config :logger, :console,
