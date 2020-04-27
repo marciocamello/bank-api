757e89a5ff81bd76eacfeadc2e50a818fbbaba59
Marcio Camello <marciocamello@outlook.com>
2020-04-27 13:17:22 -0300

    merge prod

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,7 @@ config :bank_api,
 config :bank_api,
        BankApi.Repo,
        adapter: Ecto.Adapters.Postgres,
-       url: "postgres://cnlairxqwucnqq:94f93233b5a47e51bffd7bf7f72c13156ee7d72705a20ccc24ba22454a227f20@ec2-54-81-37-115.compute-1.amazonaws.com:5432/dcq2aksoqjive5",
+       url: System.get_env("DATABASE_URL"),
        ssl: true
 
 config :bank_api, ecto_repos: [BankApi.Repo]
