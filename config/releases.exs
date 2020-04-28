1d03dd97c18dda5f34691071e2a6089681f33384
Marcio Camello <marciocamello@outlook.com>
2020-04-28 10:33:59 -0300

    merge

--- config/releases.exs
+++ config/releases.exs
@@ -1,1 +1,29 @@
 import Config
+
+# Environment message
+config :bank_api,
+       message: "Production"
+
+# BankApi repo
+config :bank_api,
+       BankApi.Repo,
+       url: System.get_env("DATABASE_URL"),
+       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+       ssl: System.get_env("DATABASE_SSL")==='false'
+
+config :bank_api, ecto_repos: [BankApi.Repo]
+
+# cowboy config
+config :bank_api, cowboy_port: System.get_env("PORT")
+
+# Configures Elixir's Logger
+config :logger,
+       :console,
+       format: "$time $metadata[$level] $message\n",
+       metadata: [:request_id]
+
+config :bank_api,
+       BankApi.Auth.Guardian,
+       error_handler: BankApi.Auth.ErrorHandler,
+       issuer: "bank_api",
+       secret_key: "pSk7JYofPSxwMP0RZNNLccu2+DPJtHQLcIiK2Qk4RCK++imQoFAJTLz2WE0GpeJI"
