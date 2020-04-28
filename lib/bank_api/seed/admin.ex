257d96ecec1051f39968deb90dcbae7e30df426d
Marcio Camello <marciocamello@outlook.com>
2020-04-28 15:12:52 -0300

    merge

--- lib/bank_api/seed/admin.ex
+++ lib/bank_api/seed/admin.ex
@@ -10,7 +10,8 @@ defmodule BankApi.Seed.Admin do
       firstName: "Albus",
       lastName: "Dumbledore",
       phone: "00 0000 0000",
-      password: "123123123"
+      password: "123123123",
+      acl: "admin"
     }
 
     Users.create_user(admin)
